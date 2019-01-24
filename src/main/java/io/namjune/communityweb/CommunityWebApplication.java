13e48ff92f19f7ca444a877b4bc86b3f352d08e6
Namjun Kim <namjunemy@gmail.com>
2019-01-24 21:41:39 +0900

    project initialize settings

---
+++ src/main/java/io/namjune/communityweb/CommunityWebApplication.java
@@ -0,0 +1,14 @@
+package io.namjune.communityweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityWebApplication.class, args);
+    }
+
+}
+
