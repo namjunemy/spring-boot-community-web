78a496850d2b6dd77c1b6877e1fbf242dc97b315
Namjun Kim <namjunemy@gmail.com>
2019-02-07 19:01:47 +0900

    컨트롤러 중복제거, 로그인 href 수정

--- src/main/java/io/namjune/communityweb/controller/LoginController.java
+++ src/main/java/io/namjune/communityweb/controller/LoginController.java
@@ -2,16 +2,8 @@ package io.namjune.communityweb.controller;
 
 import io.namjune.communityweb.annotation.SocialUser;
 import io.namjune.communityweb.domain.User;
-import io.namjune.communityweb.domain.enums.SocialType;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -23,23 +15,6 @@ public class LoginController {
 
     @GetMapping(value = "/{facebook|google|kakao}/complete")
     public String loginComplete(@SocialUser User user) {
-        return "redirect:/board/list";
-    }
-
-    @GetMapping(value = "/{facebook|google|kakao}/complete")
-    public String loginComplete(HttpSession session) {
-        OAuth2Authentication authentication =
-                (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
-        Map<String, String> map = (HashMap<String, String>) authentication.getUserAuthentication().getDetails();
-        session.setAttribute("user", User.builder()
-                .name(map.get("name"))
-                .email(map.get("email"))
-                .principal(map.get("id"))
-                .socialType(SocialType.FACEBOOK)
-                .createdDate(LocalDateTime.now())
-                .build()
-        );
-
         return "redirect:/board/list";
     }
 }
--- src/main/java/io/namjune/communityweb/service/BoardService.java
+++ src/main/java/io/namjune/communityweb/service/BoardService.java
@@ -16,12 +16,11 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     public Page<Board> findBoardList(Pageable pageable) {
-        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ?
-            0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+        pageable = new PageRequest(pageable.getPageNumber() <= 0 ? 0 : pageable.getPageNumber() - 1, pageable.getPageSize());
         return boardRepository.findAll(pageable);
     }
 
     public Board findBoardByIdx(Long idx) {
-        return boardRepository.findById(idx).orElse(new Board());
+        return boardRepository.findOne(idx);
     }
 }
